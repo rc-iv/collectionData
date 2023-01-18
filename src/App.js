0f1b1e7b8a17ea516ae3d8cfb809b5bc7a39b7a1
RCIV <rayscook@gmail.com>
2023-01-18 16:05:58 -0500

    added refresh button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import CollectionTable from './components/CollectionTable/CollectionTable';
 import f20logo from './store/f20 logo.png'
+
 function App() {
+    const [collectionData, setCollectionData] = useState([])
+
+    const fetchCollectionData = useCallback( async () => {
+        const response = await fetch('https://4kvrvzvpaj.execute-api.us-east-1.amazonaws.com/items');
+        const data = await response.json();
+        // filter out all items with value of 'history' for key 'type'
+        const newArray = data.filter(function (el) {
+                return el.type === 'current'
+            }
+        );
+        console.log(newArray);
+        setCollectionData(newArray)
+
+    },[]);
+
+    useEffect(() => {
+        fetchCollectionData()
+    }, [fetchCollectionData])
     return (
         <React.Fragment>
             <div className='header'>
                 <img src={f20logo} alt="logo" height='50' width='50'/>
                 <h1>floor20.io</h1>
+                <button onClick={fetchCollectionData}>Refresh Data</button>
             </div>
 
-            <CollectionTable className='collectionTable'/>
+            <CollectionTable className='collectionTable' collectionData={collectionData}/>
         </React.Fragment>
     )
 }
--- src/components/CollectionTable/CollectionTable.js
+++ src/components/CollectionTable/CollectionTable.js
@@ -1,32 +1,12 @@
-import React, {useState, useCallback, useEffect} from 'react';
+import React from 'react';
 import {DataTable} from 'primereact/datatable';
 import {Column} from "primereact/column";
-import { TabView,TabPanel } from "primereact/tabview";
 
 
 const CollectionTable = (props) => {
-    const [collectionData, setCollectionData] = useState([])
-
-
-    const fetchCollectionData = useCallback( async () => {
-        const response = await fetch('https://4kvrvzvpaj.execute-api.us-east-1.amazonaws.com/items');
-        const data = await response.json();
-        // filter out all items with value of 'history' for key 'type'
-        const newArray = data.filter(function (el) {
-                return el.type === 'current'
-            }
-        );
-        console.log(newArray);
-        setCollectionData(newArray)
-
-    },[]);
-
-    useEffect(() => {
-        fetchCollectionData()
-    }, [fetchCollectionData])
 
     return (
-        <DataTable value={collectionData} sortField="volumeFifteenMinutes" sortOrder={-1}>
+        <DataTable value={props.collectionData} sortField="volumeFifteenMinutes" sortOrder={-1}>
             {/*<Column field="imageUrl" header="Logo"></Column>*/}
             <Column field="name" sortable header="Name"></Column>
             <Column field="floorPrice" sortable header="Floor"></Column>
