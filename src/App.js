49af2b5b4aa3bb6cd4b2288e1cb848a4c81d4b95
Ray Cook <rayscook@gmail.com>
2023-01-18 11:04:25 -0500

    added title

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import CollectionTable from './components/CollectionTable/CollectionTable';
+
 function App() {
-    return <CollectionTable> </CollectionTable>
+    return (
+        <React.Fragment>
+            <H1>Collection Table</H1>
+            <CollectionTable/>
+        </React.Fragment>
+    )
 }
 
 export default App;
