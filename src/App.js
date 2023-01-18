dc4221aa4f92409216d4a07cabb7366ee07099ce
RCIV <rayscook@gmail.com>
2023-01-18 15:04:22 -0500

    added favicon and icon

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import CollectionTable from './components/CollectionTable/CollectionTable';
-
+import f20logo from './store/f20 logo.png'
 function App() {
     return (
         <React.Fragment>
-            <h1>Collection Table</h1>
-            <CollectionTable/>
+            <div className='header'>
+                <img src={f20logo} alt="logo" height='50' width='50'/>
+                <h1>floor20.io</h1>
+            </div>
+
+            <CollectionTable className='collectionTable'/>
         </React.Fragment>
     )
 }
