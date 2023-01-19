329f5d99512ce8234d9086e8721a09a9874a736d
RCIV <rayscook@gmail.com>
2023-01-19 09:55:22 -0500

    added image column and link from collection name

--- src/components/CollectionTable/CollectionTable.js
+++ src/components/CollectionTable/CollectionTable.js
@@ -4,14 +4,26 @@ import {Column} from "primereact/column";
 
 
 const CollectionTable = (props) => {
+    const imageBodyTemplate = (rowData) => {
+        return <img src={rowData.imageUrl} height='50px' width='50px'/>
+    }
 
+    const nameLinkTemplate = (rowData) => {
+        return <a href={`https://blur.io/collection/${rowData.collectionSlug}`} target="_blank">{rowData.name}</a>
+    }
     return (
-        <DataTable value={props.collectionData} sortField="volumeFifteenMinutes" sortOrder={-1}>
-            {/*<Column field="imageUrl" header="Logo"></Column>*/}
-            <Column field="name" sortable header="Name"></Column>
-            <Column field="floorPrice" sortable header="Floor"></Column>
-            <Column field="bestCollectionBid" sortable header="Bid"></Column>
+        <DataTable value={props.collectionData}
+                   sortField="volumeFifteenMinutes"
+                   sortOrder={-1}
+                   stripedRows='true'
+                   theme='md-dark-indigo'
+        >
+            <Column field="imageUrl" header="Logo" body={imageBodyTemplate}></Column>
+            <Column field="name" sortable header="Name" body={nameLinkTemplate}></Column>
             <Column field="volumeFifteenMinutes" sortable header="15mV" headerTooltip="15 Minute Volume"></Column>
+            <Column field="floorPrice" sortable header="Floor"></Column>
+            <Column field="floorPriceOneDay" sortable header="1D Floor"></Column>
+            <Column field="bestCollectionBid" sortable header="Bid"></Column>
             <Column field="totalCollectionBidValue" sortable header="TBV" headerTooltip="Total Bid Value"></Column>
             <Column field="volumeOneDay" sortable header="1DV" headerTooltip="1 Day Volume"></Column>
         </DataTable>
